fb929368e2d7e4e31e757819fdd8f962e8a2fcec
Fahim Mahtab <fahimmahtab5@gmail.com>
2025-08-20 22:15:52 +0600

    created classes,constructor,private methods,class properties,

--- bin/assignment_2.dart
+++ bin/assignment_2.dart
@@ -1,4 +1,41 @@
-void main(){
+abstract class Animal {
+  String _name;
 
-  print("test git ");
+  Animal(this._name);
+
+  void makeSound();
+
+  String get name {
+    return _name;
+  }
+}
+
+class Lion extends Animal {
+  int _manSize;
+  Lion(this._manSize,super.name);
+
+  @override
+  void makeSound() {
+
+  }
+}
+
+class Elephant extends Animal {
+  int _trunkLength;
+  Elephant(super.name,this._trunkLength);
+
+  @override
+  void makeSound() {
+    // TODO: implement makeSound
+  }
+}
+
+class Parrot extends Animal {
+  int _vocabularySize;
+  Parrot(super.name,this._vocabularySize);
+
+  @override
+  void makeSound() {
+    // TODO: implement makeSound
+  }
 }
