bf0e547c7a774e643a75f95dae9c34b1a41efb37
Fahim Mahtab <fahimmahtab5@gmail.com>
2025-08-21 14:42:54 +0600

    checked with setters

--- bin/assignment_2.dart
+++ bin/assignment_2.dart
@@ -4,14 +4,15 @@ abstract class Animal {
   Animal(this._name);
 
   void makeSound();
-
+  @override
+  String toString();
   String get name {
     return _name;
   }
 }
 
 class Lion extends Animal {
-   int _maneSize;
+  int _maneSize;
   Lion(super._name, int maneSize) : _maneSize = maneSize {
     if (maneSize < 0) {
       throw ArgumentError("Mane size cannot be negative.");
@@ -21,6 +22,7 @@ class Lion extends Animal {
   int get maneSize {
     return _maneSize;
   }
+
   set maneSize(int newSize) {
     if (newSize < 0) {
       print("Error:  Value not changed cause maneSize  was negative.");
@@ -28,14 +30,18 @@ class Lion extends Animal {
       _maneSize = newSize;
     }
   }
+
   @override
   void makeSound() {
     print("Lion is making sound");
   }
+
+  @override
+  String toString() => "Lion: $name, Mane Size: $maneSize cm";
 }
 
 class Elephant extends Animal {
-  final int _trunkLength;
+  int _trunkLength;
   Elephant(super.name, int trunkLength) : _trunkLength = trunkLength {
     if (trunkLength < 0) {
       throw ArgumentError("trunkLength cannot be negative.");
@@ -44,63 +50,80 @@ class Elephant extends Animal {
   int get trunkLength {
     return _trunkLength;
   }
+
   set trunkLength(int newTrunkLength) {
     if (newTrunkLength < 0) {
       print("Error:  Value not changed cause trunkLength was negative.");
     } else {
-      newTrunkLength = newTrunkLength;
+      _trunkLength = newTrunkLength;
     }
   }
+
   @override
   void makeSound() {
     print("Elephant is making sound");
   }
+
+  @override
+  String toString() => "Elephant: $name, Trunk Length: $trunkLength m";
 }
 
 class Parrot extends Animal {
-  final int _vocabularySize;
-  Parrot(super.name, int vocabularySize) :_vocabularySize=vocabularySize{
-    if(vocabularySize<0){
+  int _vocabularySize;
+  Parrot(super.name, int vocabularySize) : _vocabularySize = vocabularySize {
+    if (vocabularySize < 0) {
       print("vocabularySize cannot be negative");
     }
   }
   int get vocabularySize {
     return _vocabularySize;
   }
+
   set vocabularySize(int newVocabularySize) {
     if (newVocabularySize < 0) {
       print("Error:  Value not changed cause VocabularySize was negative.");
     } else {
-      newVocabularySize = newVocabularySize;
+      _vocabularySize = newVocabularySize;
     }
   }
+
+  @override
+  String toString() => "Parrot: $name, Vocabulary Size: $vocabularySize words";
+
   @override
   void makeSound() {
-    print("Parrot is making sound");
+    print("parrot is making sound");
   }
 }
-void main(){
+
+void addAnimalSafely(List<Animal> zooAnimals, Animal Function() creator) {
+  try {
+    zooAnimals.add(creator());
+  } catch (e) {
+    print("Error adding animal: $e");
+  }
+}
+
+void main() {
   List<Animal> zooAnimals = [];
-  zooAnimals.add(Lion("Lion 1", 100));
-  zooAnimals.add(Elephant("Elephant 1", 80));
-  zooAnimals.add(Parrot("Parrot 1", 70));
+  addAnimalSafely(zooAnimals, () => Lion("Lion 1", 100));
+  addAnimalSafely(zooAnimals, () => Elephant("Elephant 1", -80));
+  addAnimalSafely(zooAnimals, () => Parrot("Parrot 1", 70));
+  addAnimalSafely(zooAnimals, () => Elephant("Elephant 2", -80));
+  print("\n **Animals**");
+  for (var animal in zooAnimals) {
+    print(animal);
+    animal.makeSound();
+  }
 
-
-  zooAnimals.add(Elephant("Elephant 2", -80));
-  zooAnimals.add(Parrot("Parrot 2", -70));
-   for(var animals in zooAnimals){
-     print("\n--- Animal Details ---");
-     print("Name: ${animals.name}");
-     if (animals is Lion){
-       print("Mane Size :${animals.maneSize} cm");
-     }
-     if (animals is Elephant){
-       print("trunkLength Size :${animals.trunkLength} m");
-     }
-     if (animals is Parrot){
-       print("vocabularySize Size :${animals.vocabularySize} ");
-     }
-     animals.makeSound();
-   }
-
+  ///Check setters !
+  print("\n- Updating values -");
+  var lion = Lion("Lion3", 40);
+  lion.maneSize = 50; // valid input
+  lion.maneSize = -10; // invalidity
+  print(lion);
+  var parrot = Parrot("Parrot2", 40);
+  parrot.vocabularySize = 50; // valid
+  parrot.vocabularySize = -10; // invalid
+  print(parrot);
 }
